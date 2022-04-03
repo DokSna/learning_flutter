d09f352759dc7296bbb3a5ce55a0aff873fcd226
Artem <31787423+DokSna@users.noreply.github.com>
2022-04-03 17:45:21 +0300

    10. MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        "Hello Flutter",
-        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.deepOrange),
-        textDirection: TextDirection.ltr,
+    const MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 30),
+          textDirection: TextDirection.ltr,
+        ),
       ),
     ),
   );
